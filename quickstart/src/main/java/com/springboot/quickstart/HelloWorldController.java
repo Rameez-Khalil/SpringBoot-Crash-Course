a5f0e7795198c7e28068d162c082de7f662a5b62
rameez.khalil <rameezkhalil23@gmail.com>
2023-12-25 11:05:53 -0800

    maven concepts

---
+++ quickstart/src/main/java/com/springboot/quickstart/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.springboot.quickstart;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping(path="/")
+    public String helloWorld() {
+        return "Hello";
+    }
+}
